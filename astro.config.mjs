14b0cffd1816a39e0bd45c6ac7b19cbc9392fa8a
Piers Walter <Piers.Walter@ibm.com>
2024-11-07 10:10:13 +0000

    Set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://blog.pierswalter.co.uk',
     integrations: [mdx(), sitemap(), tailwind(), react()],
     redirects: {
         '/blog': '/'
